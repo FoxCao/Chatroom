7c07afe8d4a22f270ba4808110a637bff852e7c8
FoxCao <371147085@qq.com>
2019-06-26 10:37:02 +0800

    功能优化chatroom1.2

--- chatroom/server/server.go
+++ chatroom/server/server.go
@@ -66,37 +66,30 @@ func checkErr(err error) {
 		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
 		//写入日志文件信息
-		log.Printf("%s\n", err.Error())
-		// fmt.Println("fail to do this!")
-		// panic(err)
-		// } else {
-		// 	//打开日志文件
-		// 	logfile, logerr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+		log.Printf("Fail to do this!:%s\n", err.Error())
+		fmt.Println("fail to do this!")
+		panic(err)
+	} else {
+		//打开日志文件
+		logfile, logerr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 
-		// 	if logerr != nil {
-		// 		fmt.Println("Fail to find", *logfile)
-		// 		os.Exit(1)
-		// 	}
-		// 	log.SetOutput(logfile)
-		// 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+		if logerr != nil {
+			fmt.Println("Fail to find", *logfile)
+			os.Exit(1)
+		}
+		log.SetOutput(logfile)
+		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-		// 	//写入日志文件信息
-		// 	log.Printf("Success do it !\n")
+		//写入日志文件信息
+		log.Printf("Success do it !\n")
 	}
 }
 
-//处理接收的消息，存入消息队列
+//处理接收的消息
 func ProcessInfo(conn net.Conn) {
 	// fmt.Println("执行到函数ProcessInfo")
-	writeToLog("执行到ProcessInfo")
 	buf := make([]byte, 1024)
-	defer func() {
-
-		if conn != nil {
-			conn.Close()
-			writeToLog("执行完ProcessInfo")
-		}
-	}()
+	defer conn.Close()
 	//轮询
 	for {
 		numOfBytes, err := conn.Read(buf)
@@ -127,7 +120,6 @@ func ProcessInfo(conn net.Conn) {
 //消费消息
 func ConsumeMessage() {
 	// fmt.Println("执行到函数ConsumeMessage")
-	writeToLog("执行到ConsumeMessage")
 	for {
 		select {
 		case message := <-messageQueue:
@@ -137,23 +129,17 @@ func ConsumeMessage() {
 			break
 		}
 	}
-
-	writeToLog("执行完ConsumeMessage")
 }
 
-//解析客户端消息
 func doProcessMessage(message string) {
 	// fmt.Println("执行到函数doProcessMessage")
-	writeToLog("执行到函数doProcessMessage")
 	contents := strings.Split(message, "#")
 	// fmt.Println(len(contents))
 	if len(contents) > 1 {
 		//获取接受者主机地址
 		addr := contents[0]
-
 		//获取发送的消息
-		//如果发送的消息中包含“#”,拼接消息
-		sendMessage := strings.Join(contents[1:], "#")
+		sendMessage := contents[1]
 
 		fmt.Println(sendMessage)
 		//处理字符串中的空格
@@ -170,29 +156,7 @@ func doProcessMessage(message string) {
 
 		}
 	}
-	writeToLog("执行完成函数doProcessMessage")
 	// fmt.Println("执行完成函数doProcessMessage")
-}
-
-//写入日志运行信息
-func writeToLog(info string) {
-	//打开日志文件
-	logFile, logErr := os.OpenFile("chatLog.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-
-	if logErr != nil {
-		fmt.Println(logErr.Error())
-	}
-	defer func() {
-		if logFile != nil {
-			logFile.Close()
-		}
-	}()
-	//设置日志模板
-	log.SetOutput(logFile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-
-	//写入日志
-	log.Printf("%s\n", info)
 }
 
 func main() {
@@ -201,14 +165,8 @@ func main() {
 
 	//c/s编程首先开启监听
 	listen_socket, err := net.Listen("tcp", "127.0.0.1:8080")
-	writeToLog("开启监听")
 	//不要忘记关闭socket
-	defer func() {
-		if listen_socket != nil {
-			listen_socket.Close()
-			writeToLog("套接字已关闭")
-		}
-	}()
+	defer listen_socket.Close()
 	checkErr(err)
 
 	fmt.Println("server is starting!")
